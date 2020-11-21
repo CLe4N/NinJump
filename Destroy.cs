1cff67fa8ef3d7122536ece35ac754d9d400ee3e
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 02:55:48 +0700

    Update Destroy.cs

--- Destroy.cs
+++ Destroy.cs
@@ -6,14 +6,14 @@ using UnityEngine.UIElements;
 
 public class Destroy : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject PlatformPrefab;
-    public GameObject SpringPrefab;
-    public GameObject BrokenPrefab;
-    public Text scoreText;
-    int randomStat;
-    int scorePoint;
-    GameObject myPlat;
+    public GameObject player; // Create public gameobject variable "player"
+    public GameObject PlatformPrefab; // Create public gameobject variable "PlatformPrefab"
+    public GameObject SpringPrefab; // Create public gameobject variable "SpringPrefab"
+    public GameObject BrokenPrefab; // Create public gameobject variable "BrokenPrefab"
+    public Text scoreText; // Create public Text variable "scoreText"
+    int randomStat; // Create int variable "randomStat"
+    int scorePoint; // Create int variable "scorePoint"
+    GameObject myPlat; // Create GameObject variable "myPlat"
     void Start()
     {
     }
@@ -23,33 +23,32 @@ public class Destroy : MonoBehaviour
 
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision) // do when this gameobject trigger collider is overlap with other trigger collider
     {
-        if (collision.tag == "Platform")
+        if (collision.tag == "Platform") // do if other trigger collider tag is "Platform"
         {
-            randomStat = Random.Range(1, 7);
-            if (randomStat > 5)
+            randomStat = Random.Range(1, 7); // " randomStat" = random number between 1 to 7
+            if (randomStat > 5) // do if number in "randomStat" is more than 5
             {
                 myPlat = (GameObject)Instantiate(SpringPrefab, new Vector2(Random.Range(-8.0f, 8.0f), player.transform.position.y + (14 + Random.Range(0.5f, 1f))), Quaternion.identity);
+                //create gameobject from SpringPrefab. x position between -8.0 to 8.0. y position = player y position + (14 + random number between 0.5 to 1). ratate = 0
             }
-            else
+            else // do if not
             {
-                if (Random.Range(1, 6) > 3)
+                if (Random.Range(1, 6) > 3) // if random number between 1 to 6 is more than 3
                 {
                     myPlat = (GameObject)Instantiate(BrokenPrefab, new Vector2(Random.Range(-8.0f, 8.0f), player.transform.position.y + (14 + Random.Range(0.5f, 1.5f))), Quaternion.identity);
+                    //create gameobject from BrokenPrefab. x position between -8.0 to 8.0. y position = player y position + (14 + random number between 0.5 to 1.5). ratate = 0
                 }
                 else
                 {
                     myPlat = (GameObject)Instantiate(PlatformPrefab, new Vector2(Random.Range(-8.0f, 8.0f), player.transform.position.y + (14 + Random.Range(0.5f, 1.5f))), Quaternion.identity);
+                    //create gameobject from PlatformPrefab. x position between -8.0 to 8.0. y position = player y position + (14 + random number between 0.5 to 1.5). ratate = 0
                 }
             }
-            Destroy(collision.gameObject);
-            scorePoint += 1;
-            scoreText.text = scorePoint.ToString();
-        }
-        else
-        {
-
+            Destroy(collision.gameObject); // destroy overlaped gameobject
+            scorePoint += 1; // "scorePoint" + 1
+            scoreText.text = scorePoint.ToString(); // text of "scoreText" = value in "scorePoint"
         }
     }
 }
