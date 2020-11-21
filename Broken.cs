c4a0c59538a7c8076d2d726e2fa98b6931a3691c
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 02:42:15 +0700

    Update Broken.cs

--- Broken.cs
+++ Broken.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class Broken: MonoBehaviour
 {
-    SpriteRenderer sp;
-    BoxCollider2D collide;
-    AudioSource JumpSFX;
-    private void Start()
+    SpriteRenderer sp; // create SpriteRenderer variable name "sp"
+    BoxCollider2D collide; // create BoxCollider2D variable name "collide"
+    AudioSource JumpSFX; // create AudioSource variable name "JumpSFX"
+    private void Start() // do on first frame
     {
-        sp = GetComponent<SpriteRenderer>();
-        collide = GetComponent<BoxCollider2D>();
-        JumpSFX = GameObject.Find("JumpSFX").GetComponent<AudioSource>();
+        sp = GetComponent<SpriteRenderer>(); // "sp" = SpriteRenderer component of this gameobject
+        collide = GetComponent<BoxCollider2D>(); // "collide" = BoxCollider2D component of this gameobject
+        JumpSFX = GameObject.Find("JumpSFX").GetComponent<AudioSource>(); // "JumpSFX" = AudioSource component of "JumpSFX" gameobject
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) // do on collide with other collider
     {
-        if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
+        if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0) // do if velocity in y axis of the collided gameobject <= 0
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 700f);
-            sp.enabled = false;
-            collide.isTrigger = true;
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 700f); // Addforce in up direction to collided gameobject = 700
+            sp.enabled = false; // enabled "sp"
+            collide.isTrigger = true; // turn collider of this gameobject to trigger
         }
     }
 }
