cf4584dea46c8fc0f70685841e4a8e0d0886c760
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 03:16:35 +0700

    Update Player_Con.cs

--- Player_Con.cs
+++ Player_Con.cs
@@ -12,7 +12,7 @@ public class Player_Con : MonoBehaviour
     public bool IsGround;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //access Rigidbody2D,SpriteRenderer,Animator of this gameobject
         sp = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
@@ -24,24 +24,24 @@ public class Player_Con : MonoBehaviour
 
     void movement()
     {
-        moveInput = Input.GetAxis("Horizontal");
-        if (moveInput != 0 || moveInput == 0)
+        moveInput = Input.GetAxis("Horizontal"); // "moveInput" = axis input "Horizontal"
+        if (moveInput != 0 || moveInput == 0) // check "moveInput" value
         {
-            if (moveInput < 0)
+            if (moveInput < 0) // if value less than 0
             {
-                sp.flipX = true;
-                anim.SetBool("Walk", true);
+                sp.flipX = true; // flip this gameobject sprite on x axis
+                anim.SetBool("Walk", true); // play walk animation
             }
-            if (moveInput > 0)
+            if (moveInput > 0) //if value more than 0
             {
-                sp.flipX = false;
-                anim.SetBool("Walk", true);
+                sp.flipX = false; // cancel flip
+                anim.SetBool("Walk", true); //  play walk animation
             }
-            if (moveInput == 0)
+            if (moveInput == 0) // if value is 0
             {
-                anim.SetBool("Walk", false);
+                anim.SetBool("Walk", false); //stop walk animation
             }
         }
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y); // velocity on x axis of this game object = "moveInput" value * "speed"
     }
 }
