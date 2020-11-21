ae881648a78d4ceb5d72e3aedbfa3b72d1e481da
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 03:25:39 +0700

    Update TutorialUi.cs

--- TutorialUi.cs
+++ TutorialUi.cs
@@ -6,16 +6,16 @@ public class TutorialUi : MonoBehaviour
 {
     void Start()
     {
-        Time.timeScale = 0;
+        Time.timeScale = 0; // timescale is equal 0
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKeyDown)
+        if(Input.anyKeyDown) // if down key button is pressed
         {
-            Destroy(this.gameObject);
-            Time.timeScale = 1;
+            Destroy(this.gameObject); // destroy this game object
+            Time.timeScale = 1; // timescale is equal 1
         }
     }
 }
