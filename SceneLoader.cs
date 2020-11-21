f636537cf705e75635b9711010b08c722a565d2b
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 03:19:54 +0700

    Update SceneLoader.cs

--- SceneLoader.cs
+++ SceneLoader.cs
@@ -9,23 +9,23 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadNextHasSound()
     {
-        YooOOOooSFX.Play();
-        Invoke("loadNextScene", 2.0f);
+        YooOOOooSFX.Play(); // play YooOOOooSFX
+        Invoke("loadNextScene", 2.0f); // Invoke method "loadNextScene" for 2 sec
     }
     public void loadNextScene()
     {
-            SceneManager.LoadScene(SceneName);
+            SceneManager.LoadScene(SceneName); // loaad scene "SceneName"
     }
 
     public void LoadThisScene()
     {
-        SceneName = SceneManager.GetActiveScene().name;
-        SceneManager.LoadScene(SceneName);
-        Time.timeScale = 1;
+        SceneName = SceneManager.GetActiveScene().name; // "SceneName" = get activee scene name 
+        SceneManager.LoadScene(SceneName); // lord scene "SceneName"
+        Time.timeScale = 1; // timescale = 1
     }
 
     public void Exit()
     {
-        Application.Quit();
+        Application.Quit(); // quit applocation
     }
 }
