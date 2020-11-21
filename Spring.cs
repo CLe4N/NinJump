527866cbf478e9c1e8a6cbc955968bbc4e2a9267
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 03:23:49 +0700

    Update Spring.cs

--- Spring.cs
+++ Spring.cs
@@ -7,14 +7,14 @@ public class Spring : MonoBehaviour
     AudioSource JumpSFX;
     private void Start()
     {
-        JumpSFX = GameObject.Find("JumpSFX").GetComponent<AudioSource>();
+        JumpSFX = GameObject.Find("JumpSFX").GetComponent<AudioSource>(); // Access AudioSource Component in "JumpSFX"
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
+        if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0) // if collided gameobject velocity is less or equal to 0
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 1000f);
-            JumpSFX.Play();
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 1000f); // Add 1000 force to up direction 
+            JumpSFX.Play(); // play "JumpSFX"
         }
     }
 }
